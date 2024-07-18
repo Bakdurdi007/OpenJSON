b83af9323167cc395eba88a2325560a6cb99f337
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2024-07-18 10:01:06 +0500

    Bu shunchaki test uchun yozilgan class.

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Test {
+    public static void main(String[] args) {
+        String s = "Java dasturlash tili";
+        String[] words = s.split(" ");
+        String lastWord = words[words.length - 1];
+        System.out.println("Oxirgi so'zning uzunligi: " + lastWord.length());
+    }
+}
+
